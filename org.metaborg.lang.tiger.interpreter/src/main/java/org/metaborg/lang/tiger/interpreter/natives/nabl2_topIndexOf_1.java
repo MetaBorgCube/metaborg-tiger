406796f854e0e5b2902711a239ec7819b5882813
Vlad Vergu <v.vergu@gmail.com>
2018-02-12 16:19:13 +0200

    Trying to get a top-level termindex generated

---
+++ org.metaborg.lang.tiger.interpreter/src/main/java/org/metaborg/lang/tiger/interpreter/natives/nabl2_topIndexOf_1.java
@@ -0,0 +1,34 @@
+package org.metaborg.lang.tiger.interpreter.natives;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.function.Supplier;
+import org.metaborg.org.metaborg.lang.tiger.interpreter.generated.terms.*;
+import org.metaborg.meta.lang.dynsem.interpreter.nabl2.NaBL2TermBuild;
+import org.metaborg.meta.lang.dynsem.interpreter.nodes.building.TermBuild;
+import org.metaborg.meta.nabl2.interpreter.*;
+import org.metaborg.meta.nabl2.stratego.*;
+import org.metaborg.meta.nabl2.terms.*;
+import org.metaborg.meta.nabl2.terms.generic.*;
+import org.spoofax.interpreter.terms.IStrategoTerm;
+import com.oracle.truffle.api.dsl.NodeChild;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.source.SourceSection;
+
+@SuppressWarnings("unused")
+@NodeChild(value = "term", type = TermBuild.class)
+public abstract class nabl2_topIndexOf_1 extends NaBL2TermBuild {
+	public nabl2_topIndexOf_1(SourceSection source) {
+		super(source);
+	}
+
+	@Specialization
+	public ITermIndexTerm doGet(org.metaborg.meta.lang.dynsem.interpreter.terms.ITerm term) {
+		TermIndex index = getTermIndex(term.getStrategoTerm());
+		return new TermIndex_2_Term(index.getResource(), 0);
+	}
+
+	public static TermBuild create(SourceSection source, TermBuild term) {
+		return nabl2_indexOf_1NodeGen.create(source, term);
+	}
+}
