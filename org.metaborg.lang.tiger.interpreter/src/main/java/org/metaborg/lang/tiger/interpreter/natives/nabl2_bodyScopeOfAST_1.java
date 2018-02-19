7c869818f45f1238da853e496b32cc43edc21ce0
Vlad Vergu <v.vergu@gmail.com>
2018-02-19 15:53:31 +0200

    Organize imports

--- org.metaborg.lang.tiger.interpreter/src/main/java/org/metaborg/lang/tiger/interpreter/natives/nabl2_bodyScopeOfAST_1.java
+++ org.metaborg.lang.tiger.interpreter/src/main/java/org/metaborg/lang/tiger/interpreter/natives/nabl2_bodyScopeOfAST_1.java
@@ -1,17 +1,10 @@
 package org.metaborg.lang.tiger.interpreter.natives;
 
-import java.util.NoSuchElementException;
-import java.util.Optional;
-import java.util.function.Supplier;
-import org.metaborg.org.metaborg.lang.tiger.interpreter.generated.terms.*;
 import org.metaborg.meta.lang.dynsem.interpreter.nabl2.NaBL2TermBuild;
 import org.metaborg.meta.lang.dynsem.interpreter.nodes.building.TermBuild;
 import org.metaborg.meta.nabl2.constraints.ast.AstProperties;
-import org.metaborg.meta.nabl2.interpreter.*;
-import org.metaborg.meta.nabl2.stratego.*;
-import org.metaborg.meta.nabl2.terms.*;
-import org.metaborg.meta.nabl2.terms.generic.*;
-import org.spoofax.interpreter.terms.IStrategoTerm;
+import org.metaborg.org.metaborg.lang.tiger.interpreter.generated.terms.IScopeTerm;
+
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.source.SourceSection;
--- org.metaborg.lang.tiger.interpreter/src/main/java/org/metaborg/lang/tiger/interpreter/natives/nabl2_phantomIndex_1.java
+++ org.metaborg.lang.tiger.interpreter/src/main/java/org/metaborg/lang/tiger/interpreter/natives/nabl2_phantomIndex_1.java
@@ -1,16 +1,9 @@
 package org.metaborg.lang.tiger.interpreter.natives;
 
-import java.util.NoSuchElementException;
-import java.util.Optional;
-import java.util.function.Supplier;
-import org.metaborg.org.metaborg.lang.tiger.interpreter.generated.terms.*;
-import org.metaborg.meta.lang.dynsem.interpreter.nabl2.NaBL2TermBuild;
 import org.metaborg.meta.lang.dynsem.interpreter.nodes.building.TermBuild;
-import org.metaborg.meta.nabl2.interpreter.*;
-import org.metaborg.meta.nabl2.stratego.*;
-import org.metaborg.meta.nabl2.terms.*;
-import org.metaborg.meta.nabl2.terms.generic.*;
-import org.spoofax.interpreter.terms.IStrategoTerm;
+import org.metaborg.org.metaborg.lang.tiger.interpreter.generated.terms.ITermIndexTerm;
+import org.metaborg.org.metaborg.lang.tiger.interpreter.generated.terms.TermIndex_2_Term;
+
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.source.SourceSection;
