cf621d1337bebb45ff3968688fbd57c5fef27fea
Vlad Vergu <v.vergu@gmail.com>
2018-10-23 19:35:14 +0300

    Implements >=

--- lang.tiger.interp.scopesandframes/src/main/java/org/metaborg/lang/tiger/interp/scopesandframes/nodes/numbers/Geq_2.java
+++ lang.tiger.interp.scopesandframes/src/main/java/org/metaborg/lang/tiger/interp/scopesandframes/nodes/numbers/Geq_2.java
@@ -1,15 +1,17 @@
-package org.metaborg.lang.tiger.interpreter.generated.terms;
+package org.metaborg.lang.tiger.interp.scopesandframes.nodes.numbers;
 
-import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.metaborg.lang.tiger.interp.scopesandframes.TigerTypesGen;
 import org.metaborg.lang.tiger.interp.scopesandframes.nodes.Exp;
+import org.metaborg.lang.tiger.interp.scopesandframes.values.IntV_1;
+import org.metaborg.lang.tiger.interp.scopesandframes.values.V;
 import org.spoofax.interpreter.core.Tools;
 import org.spoofax.interpreter.terms.IStrategoAppl;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 
 import com.oracle.truffle.api.CompilerAsserts;
-import com.oracle.truffle.api.CompilerDirectives;
-import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.object.DynamicObject;
 
 public final class Geq_2 extends Exp {
 	public final static String CONSTRUCTOR = "Geq";
@@ -21,21 +23,26 @@ public final class Geq_2 extends Exp {
 	}
 
 	private Geq_2(Exp _1, Exp _2, IStrategoTerm strategoTerm) {
-		this._1 = _1;
-		this._2 = _2;
+		this.left = _1;
+		this.right = _2;
 		this.strategoTerm = strategoTerm;
 	}
 
-	private final Exp _1;
+	@Child
+	private Exp left;
 
-	private final Exp _2;
+	@Child
+	private Exp right;
 
-	public Exp get_1() {
-		return _1;
-	}
-
-	public Exp get_2() {
-		return _2;
+	@Override
+	public V executeGeneric(VirtualFrame frame, DynamicObject currentFrame) {
+		IntV_1 left = TigerTypesGen.asIntV_1(this.left.executeGeneric(frame, currentFrame));
+		IntV_1 right = TigerTypesGen.asIntV_1(this.right.executeGeneric(frame, currentFrame));
+		if (left.get_1() >= right.get_1()) {
+			return new IntV_1(1);
+		} else {
+			return new IntV_1(0);
+		}
 	}
 
 	@TruffleBoundary
@@ -45,32 +52,6 @@ public final class Geq_2 extends Exp {
 		assert Tools.isTermAppl(term);
 		assert Tools.hasConstructor((IStrategoAppl) term, CONSTRUCTOR, ARITY);
 		return new Geq_2(Exp.create(term.getSubterm(0)), Exp.create(term.getSubterm(1)), term);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Geq_2 other = (Geq_2) obj;
-		if (_1 == null) {
-			if (other._1 != null) {
-				return false;
-			}
-		} else if (!_1.equals(other._1)) {
-			return false;
-		}
-		if (_2 == null) {
-			if (other._2 != null) {
-				return false;
-			}
-		} else if (!_2.equals(other._2)) {
-			return false;
-		}
-		return true;
 	}
 
 	private final IStrategoTerm strategoTerm;
@@ -96,31 +77,11 @@ public final class Geq_2 extends Exp {
 		final StringBuilder sb = new StringBuilder();
 		sb.append(CONSTRUCTOR);
 		sb.append("(");
-		sb.append(_1);
+		sb.append(left);
 		sb.append(", ");
-		sb.append(_2);
+		sb.append(right);
 		sb.append(")");
 		return sb.toString();
 	}
 
-	@CompilationFinal
-	private boolean hashCodeComputed;
-
-	@CompilationFinal
-	private int hashcode;
-
-	@Override
-	public int hashCode() {
-		if (!hashCodeComputed) {
-			CompilerDirectives.transferToInterpreterAndInvalidate();
-			hashcode = makeHashCode();
-			hashCodeComputed = true;
-		}
-		return hashcode;
-	}
-
-	@TruffleBoundary
-	private int makeHashCode() {
-		return new HashCodeBuilder().append(_2).append(_1).toHashCode();
-	}
 }
