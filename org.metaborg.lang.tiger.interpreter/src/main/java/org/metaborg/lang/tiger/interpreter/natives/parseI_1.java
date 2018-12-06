181f296a7fc6716786feab9d86b6f63faceebec1
Vlad Vergu <v.vergu@gmail.com>
2018-12-06 10:20:40 +0200

    Assert int parsing is not in compiled code

--- org.metaborg.lang.tiger.interpreter/src/main/java/org/metaborg/lang/tiger/interpreter/natives/parseI_1.java
+++ org.metaborg.lang.tiger.interpreter/src/main/java/org/metaborg/lang/tiger/interpreter/natives/parseI_1.java
@@ -3,6 +3,7 @@ package org.metaborg.lang.tiger.interpreter.natives;
 import org.metaborg.meta.lang.dynsem.interpreter.nodes.building.NativeOpBuild;
 import org.metaborg.meta.lang.dynsem.interpreter.nodes.building.TermBuild;
 
+import com.oracle.truffle.api.CompilerAsserts;
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.Specialization;
@@ -18,6 +19,7 @@ public abstract class parseI_1 extends NativeOpBuild {
 	@Specialization
 	@TruffleBoundary
 	public int doInt(String s) {
+		CompilerAsserts.neverPartOfCompilation();
 		return Integer.parseInt(s);
 	}
 
