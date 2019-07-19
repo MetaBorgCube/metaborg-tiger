864b35513571ccb9cc1d826fd27a60be6cd31ebc
Eelco Visser <eelcovis@gmail.com>
2019-07-19 14:33:56 +0100

    static semantics of Tiger in Statix (no distinct name checks yet)

---
+++ org.metaborg.lang.tiger.statix/src/main/strategies/org/metaborg/lang/tiger/statix/strategies/Main.java
@@ -0,0 +1,9 @@
+package org.metaborg.lang.tiger.statix.strategies;
+
+import org.strategoxt.lang.Context;
+
+public class Main {    
+    public static void init(Context context) {
+        
+    }
+}
