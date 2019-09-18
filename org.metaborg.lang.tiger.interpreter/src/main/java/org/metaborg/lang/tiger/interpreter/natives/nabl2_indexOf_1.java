a02666972ec98290aaa585276a8973a8acce0c3e
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2019-09-18 13:49:20 +0200

    Fix compilation issues.

--- org.metaborg.lang.tiger.interpreter/src/main/java/org/metaborg/lang/tiger/interpreter/natives/nabl2_indexOf_1.java
+++ org.metaborg.lang.tiger.interpreter/src/main/java/org/metaborg/lang/tiger/interpreter/natives/nabl2_indexOf_1.java
@@ -1,20 +1,14 @@
 package org.metaborg.lang.tiger.interpreter.natives;
 
-import java.util.NoSuchElementException;
-import java.util.Optional;
-import java.util.function.Supplier;
-import org.metaborg.lang.tiger.interpreter.generated.terms.*;
+import org.metaborg.lang.tiger.interpreter.generated.terms.TermIndex_2;
 import org.metaborg.meta.lang.dynsem.interpreter.nodes.building.NativeOpBuild;
 import org.metaborg.meta.lang.dynsem.interpreter.nodes.building.TermBuild;
-import mb.nabl2.interpreter.*;
-import mb.nabl2.stratego.*;
-import mb.nabl2.stratego.TermIndex;
-import mb.nabl2.terms.*;
-import mb.nabl2.terms.build.*;
-import org.spoofax.interpreter.terms.IStrategoTerm;
+
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.source.SourceSection;
+
+import mb.nabl2.terms.stratego.TermIndex;
 
 @SuppressWarnings("unused") @NodeChild(value = "term", type = TermBuild.class) public abstract class nabl2_indexOf_1 extends NativeOpBuild 
 { 
