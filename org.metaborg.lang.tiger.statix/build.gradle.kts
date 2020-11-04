a2a81f3e9527f61539775b3f329f04dac4916890
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2020-11-04 11:32:54 +0100

    Update Statix build.gradle.kts

--- org.metaborg.lang.tiger.statix/build.gradle.kts
+++ org.metaborg.lang.tiger.statix/build.gradle.kts
@@ -4,10 +4,9 @@ plugins {
   `maven-publish`
 }
 
-spoofax {
-  createPublication = true
-}
-
 ecj {
-  toolVersion = "3.20.0"
+  toolVersion = "3.21.0"
+}
+tasks.withType<JavaCompile> { // ECJ does not support headerOutputDirectory (-h argument).
+  options.headerOutputDirectory.convention(provider { null })
 }
