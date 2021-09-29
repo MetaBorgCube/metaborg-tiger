6510b98463766b00eb4ad0ee281c47c66caefa66
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2021-09-29 18:29:47 +0200

    Update gradle build file

--- org.metaborg.lang.tiger.statix/build.gradle.kts
+++ org.metaborg.lang.tiger.statix/build.gradle.kts
@@ -1,12 +1,21 @@
 plugins {
   id("org.metaborg.devenv.spoofax.gradle.langspec")
-  id("de.set.ecj") // Use ECJ to speed up compilation of Stratego's generated Java files.
   `maven-publish`
 }
 
-ecj {
-  toolVersion = "3.21.0"
+val spoofax2DevenvVersion: String by ext
+spoofaxLanguageSpecification {
+  addSourceDependenciesFromMetaborgYaml.set(false)
+  addCompileDependenciesFromMetaborgYaml.set(false)
 }
-tasks.withType<JavaCompile> { // ECJ does not support headerOutputDirectory (-h argument).
-  options.headerOutputDirectory.convention(provider { null })
+// NOTE: Should match the compile and source dependencies in metaborg.yaml,
+//  except prefixed with org.metaborg.devenv: instead of org.metaborg:
+dependencies {
+  compileLanguage("org.metaborg.devenv:org.metaborg.meta.lang.esv:$spoofax2DevenvVersion")
+  compileLanguage("org.metaborg.devenv:org.metaborg.meta.lang.template:$spoofax2DevenvVersion")
+  compileLanguage("org.metaborg.devenv:statix.lang:$spoofax2DevenvVersion")
+  compileLanguage("org.metaborg.devenv:sdf3.ext.statix:$spoofax2DevenvVersion")
+
+  sourceLanguage("org.metaborg.devenv:meta.lib.spoofax:$spoofax2DevenvVersion")
+  sourceLanguage("org.metaborg.devenv:statix.runtime:$spoofax2DevenvVersion")
 }
